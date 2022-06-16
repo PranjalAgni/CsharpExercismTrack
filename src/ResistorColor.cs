172ec2e7dc678f4ffc5940fc2ba2a0fbbc425b6f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-06-17 00:11:30 +0530

    :rainbow: solve resistor color challenge

--- src/ResistorColor.cs
+++ src/ResistorColor.cs
@@ -1,14 +1,22 @@
 using System;
-
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
+  public static int ColorCode(string color)
+  {
+    string[] colorsList = Colors();
+    for (int idx = 0; idx < colorsList.Length; idx++)
     {
-        throw new NotImplementedException("You need to implement this function.");
+      if (String.Equals(color, colorsList[idx]))
+      {
+        return idx;
+      }
     }
+    return -1;
+  }
+  public static string[] Colors()
+  {
+    string[] colorsList = { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
 
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    return colorsList;
+  }
 }
